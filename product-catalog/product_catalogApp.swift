e798265252b35fdd978acad03b29cf4ade88dde6
wentao-tao <wentao.emiya@gmail.com>
2025-09-15 08:31:47 +0800

    Initial Commit

---
+++ product-catalog/product_catalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  product_catalogApp.swift
+//  product-catalog
+//
+//  Created by Wentao Guo on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct product_catalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
